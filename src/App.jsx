9ff51368a05aebd87a5a39883b3e418adee73571
Steven Tunc <steventunc@macbookpro.mynetworksettings.com>
2025-04-03 18:56:04 -0400

    Remove NavBar/Footer imports to fix broken build

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import NavBar from './components/NavBar';
 import HeroSection from './components/HeroSection';
-import Footer from './components/Footer';
 import PersonalTraining from './pages/PersonalTraining';
 import ProgramDesign from './pages/ProgramDesign';
 import TrainerConsultation from './pages/TrainerConsultation';
@@ -10,14 +7,12 @@ import TrainerConsultation from './pages/TrainerConsultation';
 function App() {
   return (
     <Router>
-      <NavBar />
       <Routes>
         <Route path="/" element={<HeroSection />} />
         <Route path="/personal-training" element={<PersonalTraining />} />
         <Route path="/program-design" element={<ProgramDesign />} />
         <Route path="/trainer-consultation" element={<TrainerConsultation />} />
       </Routes>
-      <Footer />
     </Router>
   );
 }
